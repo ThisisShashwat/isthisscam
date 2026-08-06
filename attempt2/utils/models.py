f4b8ea2dfa60da18e9d8f4930d95c348e9c2c9e3
Shashwat Agrawal <129974594+ThisisShashwat@users.noreply.github.com>
2026-08-06 18:25:45 +0530

    models

--- attempt2/utils/models.py
+++ attempt2/utils/models.py
@@ -1,43 +1,48 @@
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field, create_engine, Relationship
 from typing import Optional
 from datetime import datetime
-import json
+from sqlalchemy import Column, JSON
+
 
 class Messages(SQLModel, table=True):
     id: str = Field(primary_key=True)
     thread_id: str = Field(index=True)
     user_id: Optional[str] = Field(default=None, index=True)
 
-    timestamp: datetime = Field(default=None, index=True)
+    timestamp: datetime = Field(index=True)
 
-    is_sent_by_viewer: bool = Field(default=None, index=True)
+    is_sent_by_viewer: bool = Field(index=True)
     item_type: Optional[str] = Field(default=None, index=True)
 
-    text: Optional[str] = Field(default=None, index=True)
-    reply_id: Optional[str] = Field(foreign_key="messages.id", index=True)
+    text: Optional[str] = Field(default=None)
+    reply_id: Optional[str] = Field(default=None, foreign_key="messages.id", index=True)
 
-    reactions: Optional[list] #list
-    media #list?
+    reactions: list["Reactions"] = Relationship(back_populates="message")
+    media: list["Media"] = Relationship(back_populates="message")
+
 
 class Reactions(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     message_id: str = Field(foreign_key="messages.id", index=True)
-    emoji: str = Field(default=None, index=True)
+
     sender_id: Optional[str] = Field(default=None, index=True)
-    is_sent_by_viewer: bool = Field(default=None, index=True)
-    timestamp: datetime = Field(default=None, index=True)
+    is_sent_by_viewer: bool = Field(index=True)
+
+    emoji: str = Field(index=True)
+    timestamp: datetime = Field(index=True)
+
+    message: Optional[Messages] = Relationship(back_populates="reactions")
+
 
 class Media(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     message_id: str = Field(foreign_key="messages.id", index=True)
-    media_type ( reel/post, image, video, audio, sticker)
-    title
-    url [list]
-    ai summary
-    local_file_path
 
 
-DB_FILE = "messages.db"
+    item_type: Optional[str] = Field(default=None, index=True)
+    title: Optional[str] = Field(default=None)
+    url: Optional[list] = Field(default=None, sa_column=Column(JSON))
+    ai_summary: Optional[str] = Field(default=None)
+    local_file_path: Optional[str] = Field(default=None)
 
-engine = create_engine(f"sqlite:///{DB_FILE}")
-SQLModel.metadata.create_all(engine)
+    message: Optional[Messages] = Relationship(back_populates="media")
