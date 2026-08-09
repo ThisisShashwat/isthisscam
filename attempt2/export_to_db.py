d31bc79e976afb605699a31dadfd4f9f07f44065
Shashwat Agrawal <129974594+ThisisShashwat@users.noreply.github.com>
2026-08-10 02:55:29 +0530

    optimised db

--- attempt2/export_to_db.py
+++ attempt2/export_to_db.py
@@ -4,18 +4,23 @@ from utils.db_utils import engine, init_db
 from utils.extraction import extract_message
 from utils.insta_utils import get_client, get_thread_id, fetch_new_messages
 
-cl = get_client()
-
 # THREAD_ID = get_thread_id(cl)
 THREAD_ID = 340282366841710301281153262129164814061
 
-init_db()
 
-with Session(engine) as session:
-    new_messages = fetch_new_messages(THREAD_ID, cl, session)
+def ingest_new_messages(cl, thread_id):
+    with Session(engine) as session:
+        new_messages = fetch_new_messages(thread_id, cl, session)
 
+    extract_messages = []
     for msg in new_messages:
-        print(msg.id)
-        extracted_message = extract_message(msg, cl, media_download=True, media_summary=True)
-        session.add(extracted_message)
-    session.commit()
+        extract_messages.append(extract_message(msg, cl, media_download=True, media_summary=True))
+
+    with Session(engine) as session:
+        with session.begin():
+            session.add_all(extract_messages)
+
+
+cl = get_client()
+init_db()
+ingest_new_messages(cl, THREAD_ID)
